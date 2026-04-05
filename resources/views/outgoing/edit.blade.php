67d006dd9f7bd2466ce7170d8c3f4801734f0215
Currents <currentsaisyah@gmail.com>
2026-04-05 17:33:28 +0700

    fix UI Transcation

--- resources/views/outgoing/edit.blade.php
+++ resources/views/outgoing/edit.blade.php
@@ -52,11 +52,6 @@
     </form>
 </div>
 @endsection
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Outgoing - Dainty Dream</title>
-    <style>
         * {
             margin: 0;
             padding: 0;
